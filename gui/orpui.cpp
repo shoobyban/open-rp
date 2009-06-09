6111f0bd178d6f619bb2abd72507b5f6dab61e10
Darryl Sokoloski <darryl242@gmail.com>
2009-06-09 14:36:45 +0000

    Reduced list-box height.

--- gui/orpui.cpp
+++ gui/orpui.cpp
@@ -142,7 +142,7 @@ orpUIFrame::orpUIFrame(const wxString& title)
 	orpUIPanel *panel = new orpUIPanel(this);
 
 	lb = new wxListCtrl(panel,
-		orpID_LIST, wxDefaultPosition, wxSize(-1, 160),
+		orpID_LIST, wxDefaultPosition, wxSize(-1, 120),
 		wxBORDER_SUNKEN | wxLC_REPORT | wxLC_SINGLE_SEL | wxLC_HRULES);
 
 	lb->InsertColumn(0, _T("Name"));
