660ce4f2bc1f0a11ec035b2455deecef285676fd
Darryl Sokoloski <darryl242@gmail.com>
2009-11-19 16:02:42 +0000

    SVN snapshot will not be uploaded if it already exists.

--- util/mksnap.sh
+++ util/mksnap.sh
@@ -8,8 +8,10 @@ archives=$(ls ORP*.zip | grep -v ORP-SVN)
 for archive in $archives; do
 	os=$(echo $archive | sed -e 's/^.*-SVN-\(.*\)\.zip$/\1/')
 	snap=ORP-SVN-$svnrv-$os.zip
+	[ -f $snap ] && break
 	mv -v $archive $snap || exit 1
 	scp -i $HOME/.ssh/id_rsa $snap orp@orp.ps3-hacks.com:
 	ssh -i $HOME/.ssh/id_rsa orp@orp.ps3-hacks.com mv $snap /tmp
+	break
 done
 exit 0
